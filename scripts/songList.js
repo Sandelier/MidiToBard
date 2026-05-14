c5de4a39547d44d2a5bfcb01f1c4b447de4bdf80
Sandelier <miika.saily@gmail.com>
2026-05-14 16:40:19 +0300

    Added 223 new songs, mostly from "Sesh Midi Vault", along with 29 songs from all around the web.
    Added "Nursery", "Military", "Jazz", "Folk" and "EDM" genres
    
    "songStatistics" now has "timestamp" and "artist" has been renamed to "source"
    
    Played songs show grey buttons
    Added "New" label to highlight new songs
    Made an status filter with "Played", "Unplayed", "New"
    Genres and status panels now hide after clicking away
    
    Fixed an issue where a song could not be replayed from the start after finishing
    
    Refactored song list to use event delegation, which reduced the number of listeners by "all songs * 2 - 1" amounth
    
    Changed the initial sorting logic so that songs are now sorted by timestamp isntead of genre

--- scripts/songList.js
+++ scripts/songList.js
@@ -11,15 +11,29 @@ export async function main() {
 	if (songsAdded) return;
 
 	const response = await fetch("./songs/songStatistics.json");
-	let songStatistics = await response.json();
+	const rawSongStatistics = await response.json();
 
-	songStatistics = Object.fromEntries(
-		Object.entries(songStatistics).sort((a, b) => {
-			const genreA = a[1].genre.toLowerCase();
-			const genreB = b[1].genre.toLowerCase();
-			return genreA.localeCompare(genreB);
-		})
-	);
+	const songStatistics = {};
+
+	const now = Date.now();
+	const week = 1000 * 60 * 60 * 24 * 7;
+
+	const sortedTimestamps = Object.keys(rawSongStatistics)
+		.map(Number)
+		.sort((a, b) => b - a);
+
+	for (const timestamp of sortedTimestamps) {
+		const songs = rawSongStatistics[timestamp];
+		const isNew = (now - timestamp) < week;
+
+		for (const [songPath, songData] of Object.entries(songs)) {
+			songStatistics[songPath] = {
+				...songData,
+				newSong: isNew,
+				addedAt: timestamp
+			};
+		}
+	}
 
 	addSongs(songStatistics);
 }
@@ -28,6 +42,7 @@ export async function main() {
 function addSongs(songStatistics) {
 	const fragment = document.createDocumentFragment();
 	const genresMap = new Map();
+	const playedSongs = JSON.parse(localStorage.getItem("playedSongs") || "{}");
 
 	Object.entries(songStatistics).forEach(([filePath, data], index) => {
 
@@ -39,14 +54,16 @@ function addSongs(songStatistics) {
 
 		const songName = filePath
 			.replace("list\\", "")
+			.replace("Sesh Midi Vault\\", "")
 			.replace(".json", "");
 
 		const songPath = "./songs/" + filePath;
 
 		const songEle = createDiv("", "song");
-		songEle.dataset.filePath = songPath;
+		songEle.dataset.songPath = songPath;
+		songEle.dataset.songName = songName;
 
-		// Info
+        // Info
 		const info = createDiv("", "songInfo");
 
 		const titleContainer = createDiv("", "songTitleContainer");
@@ -54,9 +71,8 @@ function addSongs(songStatistics) {
 		const indexEle = createDiv(`#${index + 1}`, "songIndex");
 		titleContainer.append(title, indexEle);
 
-		const artist = createDiv(data.artist, "songArtist");
-
-		info.append(titleContainer, artist);
+		const source = createDiv(data.source, "songSource");
+		info.append(titleContainer, source);
 
 		// Meta
 		const meta = createDiv("", "songMeta");
@@ -70,57 +86,22 @@ function addSongs(songStatistics) {
 
 		meta.append(genre, duration, notes);
 
+		if (data.newSong) {
+			meta.append(createDiv("New", "songNew"));
+		}
+
 		// Controls
 		const controls = createDiv("", "songControls");
 
-		const playbackBtn = createButton("", async () => {
-			if (isLoadingSong) return;
-			isLoadingSong = true;
-			try {
-				const songNotes = await loadSong(songPath);
-			
-				if (songPath !== currentSongPath) {
-					currentSongPath = songPath;
-				
-					pauseSong();
-					timeline = [];
-					pauseTime = 0;
-					scheduledIndex = 0;
-				
-					document.querySelectorAll('.songControls .playbackBtn.playing').forEach(btn => {
-						btn.classList.remove('playing');
-						btn.closest('.song').style.backgroundColor = "";
-					});
-				
-					await playSong(songNotes);
-				
-					playbackBtn.classList.add("playing");
-					songEle.style.backgroundColor = "#2a2a2a";
-				
-					return;
-				}
-			
-				if (isPlaying) {
-					pauseSong();
-					document.querySelectorAll('.songControls .playbackBtn.playing').forEach(btn => {
-						btn.classList.remove('playing');
-						btn.closest('.song').style.backgroundColor = "";
-					});
-				
-				} else {
-					await playSong(songNotes);		
-					playbackBtn.classList.add("playing");
-					songEle.style.backgroundColor = "#2a2a2a";
-				}
-			
-				} finally {
-					isLoadingSong = false;
-				}
-		});
+		const playbackBtn = document.createElement("button");
+		playbackBtn.className = "playbackBtn";
+
+		if (playedSongs[songPath]) {
+			playbackBtn.classList.add("playedSong");
+		}
 
 		playbackBtn.setAttribute("aria-label", `Playback ${songName}`);
 
-		playbackBtn.classList.add("playbackBtn");
 		playbackBtn.innerHTML = `
 			<svg class="songPlaybackSvg play" viewBox="0 0 12 24">
 				<polygon points="0,5 12,12 0,19"></polygon>
@@ -131,17 +112,9 @@ function addSongs(songStatistics) {
 			</svg>
 		`;
 
-		const addBtn = createButton("+", async () => {
-			const songNotes = await loadSong(songPath);
-
-			(await import("./noteProcessor.js")).directProcess(
-				songNotes,
-				rawSongData,
-				songName
-			);
-
-			document.getElementById("closeOverlay").click();
-		});
+		const addBtn = document.createElement("button");
+		addBtn.className = "addBtn";
+		addBtn.textContent = "+";
 
 		controls.append(playbackBtn, addBtn);
 
@@ -152,25 +125,161 @@ function addSongs(songStatistics) {
 	songListEle.appendChild(fragment);
 	songsAdded = true;
 
-	// Genre btns
+	songListEle.addEventListener("pointerup", async (e) => {
+		const btn = e.target.closest("button");
+
+		if (!btn) return;
+
+		e.stopPropagation();
+
+		const songEle = btn.closest(".song");
+
+		if (!songEle) return;
+
+		const songPath = songEle.dataset.songPath;
+		const songName = songEle.dataset.songName;
+
+		if (btn.classList.contains("playbackBtn")) {
+
+			if (isLoadingSong) return;
+			isLoadingSong = true;
+
+			try {
+				const songNotes = await loadSong(songPath);
+				playedSongs[songPath] = true;
+				localStorage.setItem("playedSongs", JSON.stringify(playedSongs));
+				btn.classList.add("playedSong");
+
+				if (songPath !== currentSongPath) {
+					currentSongPath = songPath;
+
+					pauseSong();
+					timeline = [];
+					pauseTime = 0;
+					scheduledIndex = 0;
+
+					document.querySelectorAll(".playbackBtn.playing").forEach(playBtn => {
+						playBtn.classList.remove("playing");
+						playBtn.closest(".song").style.backgroundColor = "";
+					});
+
+					await playSong(songNotes);
+
+					btn.classList.add("playing");
+					songEle.style.backgroundColor = "#2a2a2a";
+
+					return;
+				}
+
+				if (isPlaying) {
+					pauseSong();
+					document.querySelectorAll(".playbackBtn.playing").forEach(playBtn => {
+						playBtn.classList.remove("playing");
+						playBtn.closest(".song").style.backgroundColor = "";
+					});
+
+				} else {
+					await playSong(songNotes);
+					btn.classList.add("playing");
+					songEle.style.backgroundColor = "#2a2a2a";
+				}
+
+			} finally {
+				isLoadingSong = false;
+			}
+
+		} else if (btn.classList.contains("addBtn")) {
+
+			const songNotes = await loadSong(songPath);
+
+			(await import("./noteProcessor.js")).directProcess(
+				songNotes,
+				rawSongData,
+				songName
+			);
+
+			document.getElementById("closeOverlay").click();
+		}
+	});
+
+	songListEle.appendChild(fragment);
+	songsAdded = true;
+
+	const genresPanel = document.getElementById("genresPanel");
+	const statusPanel = document.getElementById("statusPanel");
+
+	// Filter tab toggle
+
+	document.addEventListener("click", (e) => {
+		const panelClick = [statusPanel, genresPanel].some(panel => panel.contains(e.target));
+		if (!panelClick) {
+			statusPanel.classList.remove("active");
+			genresPanel.classList.remove("active");
+
+			document.querySelectorAll(".filterTab").forEach(t => t.classList.remove("active"));
+		}
+	});
+
+
+	const filterTabs = document.querySelectorAll(".filterTab");
+	const filterPanels = document.querySelectorAll(".filterPanel");
+
+	filterTabs.forEach(tab => {
+		tab.addEventListener("click", (e) => {
+			e.stopPropagation();
+		
+			const targetPanel = document.getElementById(tab.dataset.panel);
+			const isActive = targetPanel.classList.contains("active");
+		
+			filterPanels.forEach(panel => panel.classList.remove("active"));
+			filterTabs.forEach(t => t.classList.remove("active"));
+		
+			if (!isActive) {
+				targetPanel.classList.add("active");
+				tab.classList.add("active");
+			}
+		});
+	});
+
+	// Status Filter
+
+	const statusButtons = statusPanel.querySelectorAll("button");
+
+	statusButtons.forEach(btn => {
+		btn.addEventListener("click", () => {
+
+			const isActive = btn.classList.contains("active");
+			statusButtons.forEach(btn => btn.classList.remove("active"));
+
+			if (!isActive) {
+				btn.classList.add("active");
+				statusFilter = btn.textContent.toLowerCase();
+			} else {
+				statusFilter = null;
+			}
+
+			searchInput.dispatchEvent(new Event("input"));
+		});
+	});
+
+	// Genre Filter
 
 	const sortedGenres = [...genresMap.entries()]
 		.sort((a, b) => b[1] - a[1]);
 
-	const genreContainer = document.getElementById("genreButtons");
 
 	const allBtn = createButton("All", () => {
 		searchInput.value = "";
 		searchInput.dispatchEvent(new Event("input"));
 	});
-	genreContainer.appendChild(allBtn);
+	genresPanel.appendChild(allBtn);
 	sortedGenres.forEach(([genre]) => {
 		const btn = createButton(genre, () => {
 			searchInput.value = genre;
 			searchInput.dispatchEvent(new Event("input"));
 		});
 
-		genreContainer.appendChild(btn);
+		genresPanel.appendChild(btn);
 	});
 
 	allGenres = sortedGenres.map(g => g[0].toLowerCase());
@@ -210,6 +319,7 @@ function createButton(text, onClick) {
 // Search
 const searchInput = document.getElementById("songSearch");
 let allGenres = null;
+let statusFilter = null;
 
 searchInput.addEventListener("input", () => {
 	const query = searchInput.value.toLowerCase();
@@ -219,15 +329,41 @@ searchInput.addEventListener("input", () => {
 
 	songs.forEach(song => {
 		const title = song.querySelector(".songTitle").textContent.toLowerCase();
-		const artist = song.querySelector(".songArtist").textContent.toLowerCase();
+		const source = song.querySelector(".songSource").textContent.toLowerCase();
 		const genre = song.querySelector(".songGenre").textContent.toLowerCase();
+
+		if (statusFilter) {
+			const newSong = song.querySelector(".songNew");
+			const isPlayed = song.querySelector(".playedSong");
+
+			if (statusFilter === "new") {
+				if (!newSong) {
+					song.style.display = "none";
+					return;
+				}
+			}
+
+			if (statusFilter === "played") {
+				if (!isPlayed) {
+					song.style.display = "none";
+					return;
+				}
+			}
+
+			if (statusFilter === "unplayed") {
+				if (isPlayed) {
+					song.style.display = "none";
+					return;
+				}
+			}
+		}
 
 		if (isGenre) {
 			song.style.display = genre === query ? "" : "none";
 			return;
 		}
 
-		if (title.includes(query) || artist.includes(query)) {
+		if (title.includes(query) || source.includes(query)) {
 			song.style.display = "";
 		} else {
 			song.style.display = "none";
@@ -321,6 +457,8 @@ function scheduler() {
 
 	if (scheduledIndex >= timeline.length) {
 		pauseSong();
+		pauseTime = 0;
+		scheduledIndex = 0;
 	}
 }
 
